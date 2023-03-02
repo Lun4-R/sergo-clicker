bf05317f99a8fc14fd5f68acad872766c5c0e0ab
Niko Steiner <80452066+Lun4-R@users.noreply.github.com>
2023-03-02 23:13:53 +0200

    v0.1

---
+++ js/Layer0.js
@@ -0,0 +1,62 @@
+addLayer("A", {
+    name: "Argumendid", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "ARG", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#b78dff",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "Argumendid", // Name of prestige currency
+    baseResource: "hälvari momendid", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    upgrades: {
+        11: {
+            title: "Degeneratsioon",
+            cost: 1,
+            description: "Tooda rohkem hälvari momente",
+            effect() {
+                return new Decimal(3)
+            }
+        },
+        12: {
+            title: "Autism",
+            cost: 5,
+            description: "Tooda rohkem hälvari momente võttes aluseks hälvari momente",
+            effect() {
+                let base = player.points
+                let pow = 0.25
+                let eff = new Decimal.pow(base, pow).add(1)
+                return eff;
+            }
+        }
+    },
+    tabFormat: {
+        "Main" : {
+            unlocked() {return true},
+            content : [
+                "main-display",
+                "resource-display",
+                "prestige-button",
+                "upgrades",
+                "buyables"
+            ]
+        },
+       
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
